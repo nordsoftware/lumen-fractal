7158568ca928dce7db8d01a6c71bac2e4e8e38f4
Kenneth Söderlund <kenneth.soderlund@nordsoftware.com>
2016-04-08 13:19:53 +0300

    Add testItem

--- tests/unit/FractalTest.php
+++ tests/unit/FractalTest.php
@@ -1,23 +1,57 @@
 <?php
 
+use Illuminate\Support\Collection;
+use Nord\Lumen\Fractal\FractalService;
+use Nord\Lumen\Tests\Files\Book;
+use Nord\Lumen\Tests\Files\BookTransformer;
 
+/**
+ * Class FractalTest.
+ */
 class FractalTest extends \Codeception\TestCase\Test
 {
+
+    use \Codeception\Specify;
+
     /**
      * @var \UnitTester
      */
     protected $tester;
 
-    protected function _before()
+    /**
+     * @var \Nord\Lumen\Fractal\FractalService
+     */
+    protected $service;
+
+    /**
+     * Tests the fractal item.
+     */
+    public function testItem()
     {
+        $this->service = new FractalService();
+
+        $book = new Book();
+        $book->setTitle('Test Book');
+        $book->setAuthor('Test Author');
+        $book->setPublisher('Test Publisher');
+
+        $data = $this->service->item($book, new BookTransformer())->toArray();
+
+        $this->specify('key "data" is required', function () use ($data) {
+            verify($data)->hasKey('data');
+        });
+
+        $bookData = $data['data'];
+
+        $this->specify('book title equals "Test Book"', function () use ($bookData) {
+            verify($bookData['title'])->equals('Test Book');
+        });
     }
 
-    protected function _after()
-    {
-    }
-
-    // tests
-    public function testMe()
+    /**
+     * Tests the fractal collection.
+     */
+    public function testCollection()
     {
 
     }
