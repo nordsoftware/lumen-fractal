1b0b279777ebe498b68d3c2d83169b9e6702a68e
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-11-28 00:39:31 +0200

    Add visibility modifier to COǸFIG_KEY

--- src/FractalServiceProvider.php
+++ src/FractalServiceProvider.php
@@ -10,7 +10,7 @@ use Nord\Lumen\Fractal\Contracts\FractalService as FractalServiceContract;
 class FractalServiceProvider extends ServiceProvider
 {
 
-    const CONFIG_KEY = 'fractal';
+    private const CONFIG_KEY = 'fractal';
 
     /**
      * @inheritdoc
